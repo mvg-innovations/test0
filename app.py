d1cc5cc148dd2974a92e9c83518fbaa690adb455
MVG-Innovations <83455446+mvg-innovations@users.noreply.github.com>
2021-06-13 15:30:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+#Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+
+from flask import Flask
+
+#Flask constructor takes the name of
+# current module (__name__) as argument.
+
+app = Flask(__name__)
+app.debug = True
+
+#The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+
+def hello_world():
+    return 'hello'
+
+# main driver function
+if __name__ == '__main__':
+    app.run()
+# run() method of Flask class runs the application
+    # on the local development server.
